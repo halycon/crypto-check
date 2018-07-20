3f943af7efa923c2af358dc91b60618752850e66
eren altan <erenaltan@gmail.com>
2018-07-20 09:17:42 +0100

    server ports conf

--- zuul-gateway-service/src/main/java/com/cryptocheck/zuul/CryptoCheckZullApplication.java
+++ zuul-gateway-service/src/main/java/com/cryptocheck/zuul/CryptoCheckZullApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class CryptoCheckZullApplication {
 
 	public static void main(String[] args) {
